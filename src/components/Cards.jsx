91c765c645ae5ac582f1a3cb786b19b549736772
Charlton Shih <168577923+chewton2k@users.noreply.github.com>
2025-06-14 03:11:02 -0700

    changed card sytle

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,130 +1,41 @@
 import './Cards.css'
 
 const Cards = () => {
+  const imageNames = [
+    "disc",
+    "foreheadwteeth",
+    "glasses",
+    "helmet",
+    "lastday",
+    "lockin",
+    "mofusand",
+    "mybed",
+    "peace",
+    "shoulder",
+    "sleepingwsmiski",
+    "studysession",
+    "undierun"
+  ];
+
   return (
-    <div className="flex flex-wrap gap-8"> 
-      <div className="card">
-        {/* Image goes here */}
-        <img  
-          src="/Portfolio/chunckysquirrel.JPG" 
-          alt="Chunky Squirrel" 
-          className="card__image w-full h-full object-cover"
-        />
-        <div className="card__content">
-          <p className="card__title">Chunky Squirrel</p>
-          <p className="card__description">
-            Big Squirrel caught on campus eating food
-          </p>
+    <div className="flex flex-wrap gap-8 justify-center">
+      {imageNames.map((name) => (
+        <div className="flip-card" key={name}>
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img 
+                src={`/${name}.jpeg`}
+                alt={name}
+                className="rounded-[1rem] w-[200px] h-[200px] object-cover"
+              />
+            </div>
+            <div className="flip-card-back">
+              <p className="title">BACK</p>
+              <p>Leave Me</p>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="card">
-        {/* Image goes here */}
-        <img 
-          src="/Portfolio/powell.jpeg" 
-          alt="Powell" 
-          className="card__image w-full h-full object-cover"
-        />
-        <div className="card__content">
-          <p className="card__title">Rainy Powell</p>
-          <p className="card__description">
-            Nice picture to capture emotions
-          </p>
-        </div>
-      </div>
-
-      <div className="card">
-        {/* Image goes here */}
-        <img 
-          src="/Portfolio/uclafire.jpeg" 
-          alt="UCLA fire" 
-          className="card__image w-full h-full object-cover"
-        />
-        <div className="card__content">
-          <p className="card__title">Fire</p>
-          <p className="card__description">
-            When the fire and smoke was so bad but still had classes 
-          </p>
-        </div>
-      </div>
-
-      <div className="card">
-        {/* Image goes here */}
-        <img 
-          src="/Portfolio/studybusiness.jpeg" 
-          alt="Studying" 
-          className="card__image w-full h-full object-cover"
-        />
-        <div className="card__content">
-          <p className="card__title">Studying</p>
-          <p className="card__description">
-            Me studying with my best friend chatgpt!
-          </p>
-        </div>
-      </div>
-
-      <div className="card">
-        {/* Image goes here */}
-        <img 
-          src="/Portfolio/bittybite.jpeg" 
-          alt="Bittybites" 
-          className="card__image w-full h-full object-cover"
-        />
-        <div className="card__content">
-          <p className="card__title">Bites</p>
-          <p className="card__description">
-            One of the best dessert places on campus; warm donuts with ice cream. 
-          </p>
-        </div>
-      </div>
-
-      <div className="card">
-        {/* Image goes here */}
-        <img 
-          src="/Portfolio/gymday.jpeg" 
-          alt="Gym Day" 
-          className="card__image w-full h-full object-cover"
-        />
-        <div className="card__content">
-          <p className="card__title">Gym</p>
-          <p className="card__description">
-            Trying the ucla gyms for the first time. 
-          </p>
-        </div>
-      </div>
-
-      <div className="card">
-        {/* Image goes here */}
-        <img 
-          src="/Portfolio/fountain.jpeg" 
-          alt="Fountain at night" 
-          className="card__image w-full h-full object-cover"
-        />
-        <div className="card__content">
-          <p className="card__title">First 12 am walk</p>
-          <p className="card__description">
-            The cold depressing night walk 
-          </p>
-        </div>
-      </div>
-
-      <div className="card">
-        {/* Image goes here */}
-        <img 
-          src="/Portfolio/tennis.jpg" 
-          alt="Tennis with Friends" 
-          className="card__image w-full h-full object-cover"
-        />
-        <div className="card__content">
-          <p className="card__title">Playing Tennis</p>
-          <p className="card__description">
-            One of the last hitting sessions I had with Son and Kyle  
-          </p>
-        </div>
-      </div>
-
-
-
+      ))}
     </div>
   );
 };
